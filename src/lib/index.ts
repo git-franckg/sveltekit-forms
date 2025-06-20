d387653d3f48dfb63976d43d8095a06f26590071
Franck GOGO <franck.gogo@outlook.com>
2025-06-20 16:45:29 +0200

    v0.4.1: expose type of class Behavior

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,6 @@
-export type { BehaviorOptions } from './behavior.svelte.ts';
+// Behavior should not created by the user, it is automatically created in Form.
+export type { Behavior, BehaviorOptions } from './behavior.svelte.ts';
+
 export { Form, type FormInput, type FormConfig } from './form.svelte.js';
+
 export { Participant, type ParticipantConfig, type ParticipantInput } from './participant.svelte.js';
